8e3ed66bf3e97609a29f4a63a5a2dbe89c629d72
Daniel Barrett <barrett.t.daniel@gmail.com>
2022-05-28 23:08:27 +1000

    add global settings page

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,51 @@
-import ForgeUI, { render, Fragment, Macro, Text } from '@forge/ui';
+import {webTrigger} from "@forge/api"
+import ForgeUI, { Fragment, GlobalSettings, render, Text, useEffect, useState } from "@forge/ui"
+
+export const run = async () => {
+  console.log("running")
+  try {
+    const url = await webTrigger.getUrl("triggerA")
+    console.log("get url success", url)
+    return {
+      statusCode: 200
+    }
+  } catch (err) {
+    console.log("get url failure", err)
+    return {
+      statusCode: 500
+    }
+  }
+}
+
+const settings = async (): Promise<[string, string]> => {
+  try {
+    const url = await webTrigger.getUrl("triggerA")
+    console.log("get url success", url)
+    return ["success", url]
+  } catch (err) {
+    console.log("get url failure", err)
+    return ["failure", JSON.stringify(err)]
+  }
+}
 
 const App = () => {
+  const [url, setUrl] = useState("")
+  const [state, setState] = useState("")
+  useEffect(async () => {
+    const [newState, newUrl] = await settings()
+    setState(newState)
+    setUrl(newUrl)
+  }, [])
   return (
     <Fragment>
-      <Text>Hello world!</Text>
+      <Text>Trigger Url: {url}</Text>
+      <Text>State: {state}</Text>
     </Fragment>
-  );
+  )
 };
 
-export const run = render(
-  <Macro
-    app={<App />}
-  />
+export const runSettings = render(
+  <GlobalSettings>
+    <App />
+  </GlobalSettings>
 );
